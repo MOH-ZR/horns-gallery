a58b7cb624c39f25dfcb828866faa53612008109
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-03-22 22:32:54 +0200

    lab-02 done

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# horns-gallery
-Horns gallery showing different types of horns filtered based on the horn's name.
+# The Gallery of Horns
+Horns gallery showing different types of horns filtered based on the horn's name. 
+
+## Time Estimate 
+
+```
+Number and name of feature: feature #1 - display images
+
+Estimate of time needed to complete: 75 minutes
+
+Start time: (3:05 pm)
+
+Finish time: (4:05 pm)
+
+Actual time needed to complete: 60 minutes 
+```  
+
+```
+Number and name of feature: feature #2 - filter images
+
+Estimate of time needed to complete: 60 minutes
+
+Start time: (4:25 pm)
+
+Finish time: (6:10 pm)
+
+Actual time needed to complete: 105 minutes 
+```  
+
+```
+Number and name of feature: feature #3 - style the application
+
+Estimate of time needed to complete: 40 minutes
+
+Start time: (8:28 pm)
+
+Finish time: (10:00 pm)
+
+Actual time needed to complete: 92 minutes 
+```
---
+++ js/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const horns = [];
+
+const Horn = function(horn) {
+    this.title = horn.title;
+    this.description = horn.description;
+    this.keyword = horn.keyword;
+    this.image_url = horn.image_url;
+    this.horns = horn.horns;
+};
+
+Horn.prototype.renderHorn = function() {
+    $('main').append(`
+        <section class="photo-template">
+            <h2>${this.title}</h2>
+            <img src="${this.image_url}" alt="">
+            <p>${this.description}</p>  
+        </section>
+    `);
+
+
+};
+
+$(function() {
+    // ajax setting 
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json',
+    };
+
+    // getting data from JSON 
+    $.ajax('../data/page-1.json', ajaxSettings).then((data) => {
+        data.forEach((horn) => {
+            let hornObject = new Horn(horn);
+            horns.push(hornObject);
+            hornObject.renderHorn();
+        });
+        renderKeywords();
+    });
+
+    // filter the content based on the selected keyword
+    $('select').on('change', () => {
+        $('main').text("");
+        let selectedVal = $(this).find(':selected').val();
+        horns.forEach((horn) => {
+            if (selectedVal == 'default') {
+                new Horn(horn).renderHorn();
+            } else if (selectedVal == horn.keyword) {
+                new Horn(horn).renderHorn();
+            };
+        });
+    });
+});
+
+
+// render the keywords list
+function renderKeywords() {
+    const tempArr = [];
+    horns.forEach((horn) => {
+        if (!tempArr.includes(horn.keyword)) {
+            tempArr.push(horn.keyword);
+            $('select').append(`<option value=${horn.keyword}>${horn.keyword}</option>`)
+        };
+    });
+}
+
+console.log(horns);
