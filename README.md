12a8ddf7d5b8fd63bc1844e6dde003220e246e64
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-03-24 06:22:58 +0200

    lab-03 solved

--- README.md
+++ README.md
@@ -1,40 +1,52 @@
 # The Gallery of Horns
-Horns gallery showing different types of horns filtered based on the horn's name. 
+Horns gallery showing different types of horns filtered based on the horn's name and sorted either by title or numer of horns available. 
 
 ## Time Estimate 
 
 ```
-Number and name of feature: feature #1 - display images
-
-Estimate of time needed to complete: 75 minutes
-
-Start time: (3:05 pm)
-
-Finish time: (4:05 pm)
-
-Actual time needed to complete: 60 minutes 
-```  
-
-```
-Number and name of feature: feature #2 - filter images
+Number and name of feature: feature #1 - Pagination
 
 Estimate of time needed to complete: 60 minutes
 
-Start time: (4:25 pm)
+Start time: (2:15 pm)
 
-Finish time: (6:10 pm)
+Finish time: (4:45 pm)
 
-Actual time needed to complete: 105 minutes 
+Actual time needed to complete: 150 minutes 
 ```  
 
 ```
-Number and name of feature: feature #3 - style the application
+Number and name of feature: feature #2 - Templating
 
-Estimate of time needed to complete: 40 minutes
+Estimate of time needed to complete: minutes
 
-Start time: (8:28 pm)
+Start time: ()
 
-Finish time: (10:00 pm)
+Finish time: ()
 
-Actual time needed to complete: 92 minutes 
+Actual time needed to complete:  minutes 
+```  
+
+```
+Number and name of feature: feature #3 - Styling with Flexbox
+
+Estimate of time needed to complete: minutes
+
+Start time: ()
+
+Finish time: ()
+
+Actual time needed to complete: minutes 
+```
+
+```
+Number and name of feature: feature #3 - Sort the images
+
+Estimate of time needed to complete: minutes
+
+Start time: ()
+
+Finish time: ()
+
+Actual time needed to complete: minutes 
 ```
--- js/app.js
+++ js/app.js
@@ -1,28 +1,71 @@
 'use strict';
 
-const horns = [];
-
-const Horn = function(horn) {
+let page1Horns = [];
+let page2Horns = [];
+let selectedPageName;
+let pageHorns = page1Horns;
+let Horn = function(horn) {
     this.title = horn.title;
     this.description = horn.description;
     this.keyword = horn.keyword;
     this.image_url = horn.image_url;
     this.horns = horn.horns;
-};
-
+}
 Horn.prototype.renderHorn = function() {
-    $('main').append(`
-        <section class="photo-template">
-            <h2>${this.title}</h2>
-            <img src="${this.image_url}" alt="">
-            <p>${this.description}</p>  
-        </section>
-    `);
-
-
+    let template = $("#horn-template").html();
+    console.log(template);
+    let html = Mustache.render(template, this);
+    $("main").append(html);
+    // $('main').append(`
+    //     <section class="photo-template">
+    //         <h2>${this.title}</h2>
+    //         <img src="${this.image_url}" alt="">
+    //         <p>${this.description}</p>  
+    //     </section>
+    // `);
 };
 
-$(function() {
+$(document).ready(function() {
+    renderPage('page-1');
+
+    // show the selected page
+    $("nav").on('click', (event) => {
+        event.preventDefault();
+
+        if ($(event.target).text() === 'page 1') {
+            selectedPageName = 'page 1';
+            page1Horns = [];
+            renderPage('page-1');
+            pageHorns = page1Horns;
+        } else if ($(event.target).text() === 'page 2') {
+            selectedPageName = 'page 2';
+            page2Horns = [];
+            renderPage('page-2');
+            pageHorns = page2Horns;
+        }
+    });
+
+    // filter the content based on the selected keyword
+    $('select').on('change', () => {
+        $('main').text("");
+        let selectedVal = $(this).find(':selected').val();
+        console.log(pageHorns.length);
+        pageHorns.forEach((horn) => {
+            if (selectedVal == 'default') {
+                (new Horn(horn)).renderHorn();
+            } else if (horn.keyword == selectedVal) {
+                (new Horn(horn)).renderHorn();
+            }
+        });
+    });
+
+});
+
+// render the selected page
+function renderPage(pageName) {
+    $('main').text("");
+    $('select').text("");
+
     // ajax setting 
     const ajaxSettings = {
         method: 'get',
@@ -30,39 +73,23 @@ $(function() {
     };
 
     // getting data from JSON 
-    $.ajax('data/page-1.json', ajaxSettings).then((data) => {
+    $.ajax(`data/${pageName}.json`, ajaxSettings).then((data) => {
         data.forEach((horn) => {
             let hornObject = new Horn(horn);
-            horns.push(hornObject);
+            pageHorns.push(hornObject);
             hornObject.renderHorn();
         });
         renderKeywords();
     });
-
-    // filter the content based on the selected keyword
-    $('select').on('change', () => {
-        $('main').text("");
-        let selectedVal = $(this).find(':selected').val();
-        horns.forEach((horn) => {
-            if (selectedVal == 'default') {
-                new Horn(horn).renderHorn();
-            } else if (selectedVal == horn.keyword) {
-                new Horn(horn).renderHorn();
-            };
-        });
-    });
-});
-
-
+}
 // render the keywords list
 function renderKeywords() {
     const tempArr = [];
-    horns.forEach((horn) => {
+    $('select').append(`<option value="default">Filter by Keyword</option>`);
+    pageHorns.forEach((horn) => {
         if (!tempArr.includes(horn.keyword)) {
             tempArr.push(horn.keyword);
-            $('select').append(`<option value=${horn.keyword}>${horn.keyword}</option>`)
-        };
+            $('select').append(`<option value=${horn.keyword}>${horn.keyword}</option>`);
+        }
     });
 }
-
-console.log(horns);
